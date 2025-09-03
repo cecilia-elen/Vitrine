0b1d1f6c9968b13cb5fa5a3ea6538992f6987e1b
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-03 14:04:53 -0300

    Delete Vitrine directory

--- Vitrine/views.py
+++
@@ -1,8 +0,0 @@
-# Dentro do arquivo Vitrine/views.py
-
-from django.shortcuts import render
-
-def pagina_inicial(request):
-    # Esta função simplesmente diz ao Django para encontrar e mostrar o arquivo 'index.html'
-    # Ele vai procurar na pasta templates que configuramos no settings.py
-    return render(request, 'index.html')
